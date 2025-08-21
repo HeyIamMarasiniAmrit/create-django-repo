6e429e424092441f0c96bda9389990f55af3911c
Amrit Marasini <101445427+HeyIamMarasiniAmrit@users.noreply.github.com>
2025-08-21 13:03:47 +0545

    Add files via upload

---
+++ views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return HttpResponse('Home page')
+
+def learn_django(request):
+    return HttpResponse('Hello Djano')
+
+
+def learn_python(request):
+    return HttpResponse('<h1>Hello python</h1>')
+
+def learn_php(request):
+    lang = '<h2>hello world</h2>'
+    return HttpResponse(lang)
