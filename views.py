8bf39cee7f2b121a29cd4be1fcd25c81cbb0c1c4
Amrit Marasini <101445427+HeyIamMarasiniAmrit@users.noreply.github.com>
2025-08-23 12:53:24 +0545

    Update views.py

--- views.py
+++ views.py
@@ -1,17 +1,26 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+# from django.shortcuts import render
+# from django.http import HttpResponse
 
-# Create your views here.
+# # Create your views here.
+# def home(request):
+#     return HttpResponse('Home page')
+
+# def learn_django(request):
+#     return HttpResponse('Hello Djano')
+
+
+# def learn_python(request):
+#     return HttpResponse('<h1>Hello python</h1>')
+
+# def learn_php(request):
+#     lang = '<h2>hello world</h2>'
+#     return HttpResponse(lang)
+
+
 def home(request):
-    return HttpResponse('Home page')
-
-def learn_django(request):
-    return HttpResponse('Hello Djano')
+    return HttpResponse('Home Page')
 
 
-def learn_python(request):
-    return HttpResponse('<h1>Hello python</h1>')
+def myapp1(request):
+    return HttpResponse('my app 1')
 
-def learn_php(request):
-    lang = '<h2>hello world</h2>'
-    return HttpResponse(lang)
