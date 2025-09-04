fafa1a0be13009587ee7060b1419c461aa69465f
Amrit Marasini <101445427+HeyIamMarasiniAmrit@users.noreply.github.com>
2025-09-04 13:55:43 +0545

    Add files via upload

--- views.py
+++ views.py
@@ -1,27 +1,25 @@
-# from django.shortcuts import render
-# from django.http import HttpResponse
+from django.shortcuts import render
+from datetime import datetime
 
-# # Create your views here.
-# def home(request):
-#     return HttpResponse('Home page')
+# Create your views here.
 
-# def learn_django(request):
-#     return HttpResponse('Hello Djano')
+# example 1
+# def learn_django(req):
+#     return render(req, 'course/django.html', context={'name':'Django'})
+#
+# # example 2
+#
+# def learn_django(req):
+#     name = 'django'
+#     duration = '4 month'
+#     seats = 10
+#     course_details = {'nm': name, 'du':duration, 'st':seats}
+#     return render(req, 'course/django.html', course_details)
 
+# ex 3 filter
+# def learn_django(req):
+#     return render(req, 'course/django.html', context={'name':'Django','desc':'django is the best'})
 
-# def learn_python(request):
-#     return HttpResponse('<h1>Hello python</h1>')
-
-# def learn_php(request):
-#     lang = '<h2>hello world</h2>'
-#     return HttpResponse(lang)
-
-
-def home(request):
-    return HttpResponse('Home Page')
-
-
-def myapp1(request):
-    return HttpResponse('my app 1')
-
-
+def learn_django(req):
+    d = datetime.now()
+    return render(req, 'course/django.html', context={'dt':d})
