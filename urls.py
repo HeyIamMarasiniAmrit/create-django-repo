6bc1ce2320a04b94bf267774b8ff07a2d7f1e31b
Amrit Marasini <101445427+HeyIamMarasiniAmrit@users.noreply.github.com>
2025-10-18 12:23:12 +0545

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,44 +1,6 @@
-"""
-URL configuration for mysite project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-# from django.contrib import admin
-# from django.urls import path
-
-# from app1 import views
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('', views.home, name='home'),
-#     path('dj/', views.learn_django, name='learn_django'),
-#     path('py/', views.learn_python, name='learn_python'),
-#     path('lp/', views.learn_php, name='learn_php'),
-# ]
-from django.contrib import admin
 from django.urls import path
-from app1 import views as ap1
-from app2 import views as ap2
+from course.views import learn_django
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', ap1.home, name='home'),
-    path('app1/', ap1.myapp1, name='myapp1'),
-    path('app2/', ap2.myapp2, name='myapp2'),
-    path('app2_me/', ap2.myapp2_me, name='myapp2_me'),
-    # path('dj/', views.learn_django, name='learn_django'),
-    # path('py/', views.learn_python, name='learn_python'),
-    # path('lp/', views.learn_php, name='learn_php'),
+    path('dj/', learn_django),
 ]
-
--- views.py
+++ views.py
@@ -1,26 +1,5 @@
 from django.shortcuts import render
-from datetime import datetime
 
 # Create your views here.
-
-# example 1
-# def learn_django(req):
-#     return render(req, 'course/django.html', context={'name':'Django'})
-#
-# # example 2
-#
-# def learn_django(req):
-#     name = 'django'
-#     duration = '4 month'
-#     seats = 10
-#     course_details = {'nm': name, 'du':duration, 'st':seats}
-#     return render(req, 'course/django.html', course_details)
-
-# ex 3 filter
-# def learn_django(req):
-#     return render(req, 'course/django.html', context={'name':'Django','desc':'django is the best'})
-
 def learn_django(req):
-    d = datetime.now()
-
-    return render(req, 'course/django.html', context={'dt':d})
+    return render(req, 'course/django.html',{'nm':'django5.x'})
