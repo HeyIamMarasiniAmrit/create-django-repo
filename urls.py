b1d6ff40864d0f05012c9b803fd895853366e0a1
Amrit Marasini <101445427+HeyIamMarasiniAmrit@users.noreply.github.com>
2026-02-24 12:24:19 +0545

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
-from course.views import learn_django
+from django.urls import path, include
+from course.views import learn_django, learn_python
 
 urlpatterns = [
-    path('dj/', learn_django),
+
+    path('dj/', learn_django, name='django'),
+    path('py/', learn_python, name='python'),
 ]
