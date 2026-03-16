f7e70069130a91842d1665f91e798df127a7f4ce
Amrit Marasini <101445427+HeyIamMarasiniAmrit@users.noreply.github.com>
2026-03-16 12:45:07 +0545

    Add files via upload

--- models.py
+++ models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class profile(models.Model):
+    name= models.CharField(max_length=30)
+    email=models.EmailField(max_length=225)
+    city=models.CharField(max_length=70)
+    roll=models.IntegerField()
+    state=models.CharField(max_length=70)
+    comment = models.CharField(max_length=70, default='nothing')
 
 
 
